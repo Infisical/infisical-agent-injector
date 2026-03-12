6f4247660096c706cdbb6be75100db476b7e23cf
Victor Hugo dos Santos <victor@infisical.com>
2026-03-12 17:30:21 -0300

    feat(upload): enhance upload script to support OCI repository and add username environment variable

--- upload-to-cloudsmith.sh
+++ upload-to-cloudsmith.sh
@@ -1,7 +1,22 @@
+#!/usr/bin/env sh
+set -e
+
 cd helm
 helm dependency update
 helm package .
+
+# Push to traditional Helm repository
 for i in *.tgz; do
     [ -f "$i" ] || break
     cloudsmith push helm --republish infisical/helm-charts "$i"
+done
+
+# Push to OCI repository
+echo "$CLOUDSMITH_API_KEY" | helm registry login helm.oci.cloudsmith.io \
+    --username "$CLOUDSMITH_USERNAME" \
+    --password-stdin
+
+for i in *.tgz; do
+    [ -f "$i" ] || break
+    helm push "$i" oci://helm.oci.cloudsmith.io/infisical/helm-charts
 done
